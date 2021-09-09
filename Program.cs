61624feeed2131de04a5dde89c89dcd280e6966b
ChugaevMark <chugaev-mark@inbox.ru>
2021-09-09 20:49:42 +0300

    first program

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторону квадрата: ");
+            int a = int.Parse(Console.ReadLine());
+            int P = 4 * a;
+            int S = a * a;
+            Console.WriteLine($"Периметр = {P}\nПлощадь = {S}");
+        }
+    }
+}
